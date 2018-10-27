cc65bfde1d92d5a0c194f123651163b9c280cb17
Armen G <gharibi.armen@yahoo.com>
2018-10-27 07:55:09 -0700

    Check token & refresh token demos

--- src/main/java/com/agharibi/oauth2/spring/AuthorizationServerConfiguration.java
+++ src/main/java/com/agharibi/oauth2/spring/AuthorizationServerConfiguration.java
@@ -3,14 +3,18 @@ package com.agharibi.oauth2.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
@@ -21,6 +25,9 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     public AuthorizationServerConfiguration() {
     }
 
@@ -30,9 +37,34 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     }
 
     @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(tokenStore());
+        tokenServices.setSupportRefreshToken(true);
+        return tokenServices;
+    }
+
+    @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder(12);
     }
+
+
+
+    /**
+     * Credentials token
+     * @POST  =  http://localhost:8081/um-webapp/oauth/token?grant_type=password&client_id=live-test&username=armen@yahoo.com&password=Secured123!
+     */
+
+    /**
+     *  Refresh token
+     * @POST  =  http://localhost:8081/um-webapp/oauth/token?grant_type=refresh_token&refresh_token=6d8eb8ff-3fd3-4047-bb19-9707ffbf4488
+     */
+
+    /**
+     * @GET  =  http://localhost:8081/um-webapp/oauth/check_token?token=1a15dc88-c3cf-4714-b3e3-5420de905b4c
+     */
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -40,9 +72,10 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
                 .inMemory()
                 .withClient("live-test")
                 .secret(encoder().encode("bGl2ZS10ZXN0"))
-                .authorizedGrantTypes("password")  // password oauth2 flow
+                .authorizedGrantTypes("password", "refresh_token")  // password oauth2 flow
                 .scopes("um-webapp")
                 .autoApprove("um-webapp")
+                .refreshTokenValiditySeconds(3600 * 24)
                 .accessTokenValiditySeconds(3600);
     }
 
@@ -51,6 +84,13 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
         endpoints
                 .tokenStore(tokenStore())
                 .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService)
                 .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.checkTokenAccess("permitAll()");
+        super.configure(security);
     }
 }
